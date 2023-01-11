3299b7330daa78235a0a48c0af0da1ba1e823e69
Yooni <74585743+LeThuNgan-20127251@users.noreply.github.com>
2023-01-12 00:44:20 +0700

    Add files via upload

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -24,5 +24,18 @@ exports.store =function(req,res){
 exports.comGa =function(req,res){
     res.sendFile(__dirname.replace('controllers', 'public') + '/store.html');
 }
+exports.banhMi =function(req,res){
+    res.sendFile(__dirname.replace('controllers', 'public') + '/storeBanhMi.html');
+}
 
+exports.banhXeo =function(req,res){
+    res.sendFile(__dirname.replace('controllers', 'public') + '/storeBanhXeo.html');
+}
+
+exports.bunBoHue =function(req,res){
+    res.sendFile(__dirname.replace('controllers', 'public') + '/storeBunBoHue.html');
+}
+exports.signIn =function(req,res){
+    res.sendFile(__dirname.replace('controllers', 'public') + '/signIn.html');
+}
 
