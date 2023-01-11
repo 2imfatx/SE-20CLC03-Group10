45d40194d371e66610b2e8c204cad018aae6f4f3
2imfatx <103380684+2imfatx@users.noreply.github.com>
2023-01-12 01:54:04 +0700

    add js

---
+++ public/scriptComSuon.js
@@ -0,0 +1,65 @@
+
+function displayItems() {
+    const xhr = new XMLHttpRequest();
+    const url = 'http://localhost:3000/api/ComSuon';
+    xhr.open("GET", url);
+    xhr.send();
+    var ComSuonStore=[];
+    xhr.onreadystatechange = (e) => {
+        var res=xhr.responseText; 
+
+        try{
+            ComSuonStore = JSON.parse(res);
+            // console.log(ComSuonStore);          
+        }catch(err){
+            console.log(err);
+        }
+       
+        var ComSuon=document.getElementById('ComSuon')
+
+        const ComSuonData=ComSuonStore;
+
+        ComSuonData.map(item=>{
+    
+            var itemCard=document.createElement('div');
+            itemCard.setAttribute('id','sellingFood');
+    
+            var itemImg=document.createElement('img');
+            itemImg.src=item.ImageStore;
+    
+            const inforDiv = document.createElement('div');
+    
+            var itemName=document.createElement('p');
+            itemName.textContent = item.StoreName;
+    
+            var itemAddr=document.createElement('p');
+            itemAddr.textContent="Địa chỉ: " + item.Adress;
+    
+            var itemDesc=document.createElement('p');
+            itemDesc.textContent="Mô tả: " +item.Description;
+    
+            var itemRating=document.createElement('p');
+            itemRating.textContent="Đánh giá: " +item.Rating;
+    
+            var itemPrice=document.createElement('p');
+            itemPrice.textContent="Giá cả: " +item.Price;
+    
+            var Button = document.createElement('button');
+            Button.setAttribute('class','addToCart');
+            Button.textContent = ' Thêm vào giỏ hàng';
+    
+            itemCard.appendChild(itemImg);
+            itemCard.appendChild(inforDiv);
+            inforDiv.appendChild(itemName);
+            inforDiv.appendChild(itemAddr);
+            inforDiv.appendChild(itemDesc);
+            inforDiv.appendChild(itemRating);
+            inforDiv.appendChild(itemPrice);
+    
+            itemCard.appendChild(Button);
+    
+            ComSuon.appendChild(itemCard);
+        })   
+    }
+}
+displayItems();
